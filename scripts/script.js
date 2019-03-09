ac87284e524f7539d19b84b6a8640c7ae4ea26ac
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-03-09 17:51:40 +0200

    backup push

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+// © Anas Abu Farraj
+// Learning Handlebars
+// March 9, 2019
+
+jQuery(document).ready(() => {
+  // change the footer message at the bottom of the page
+  jQuery('footer small').text('Ready to Manipulate!');
+
+  // jQuery("#rect")
+  //   // animate width to 200px on mouseover
+  //   .on("mouseover", () => {
+  //     jQuery("#rect").animate({ width: 200 }, 300);
+  //   })
+  //
+  //   // animate width to 100px on mouseout
+  //   .on("mouseout", () => {
+  //     jQuery("#rect").animate({ width: 100, height: 100 }, 300);
+  //   })
+  //
+  //   // increase height by 100px on click
+  //   .on("click", () => {
+  //     jQuery("#rect").animate({ height: "+=100" }, "slow");
+  //   });
+});
+// --- end of code ----
