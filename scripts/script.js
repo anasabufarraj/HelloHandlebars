ad7004f760307101fff918af398ecb98263319b0
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-03-15 12:12:59 +0200

    backup push
    (cherry picked from commit c49c9d8b788bbedd6c022d1670c4e0d192aff11c)
    
    # Conflicts:
    #	index.hbs

--- scripts/script.js
+++ scripts/script.js
@@ -2,46 +2,17 @@
 // Learning Handlebars
 // March 9, 2019
 
-const cast = {
-  characters: [
-    {
-      name: 'Jon Snow',
-      shortCode: 'jon-snow',
-      actor: 'Kit Harrington',
-      house: 'Stark',
-      location: undefined,
-    },
-    {
-      name: 'Tyrion Lannister',
-      shortCode: 'tyron',
-      actor: 'Peter Dinklage',
-      house: 'Tyron',
-      location: 'Lannister',
-    },
-    {
-      name: 'Brienne of Tarth',
-      shortCode: 'brienne',
-      actor: 'Gwendoline Christie',
-      house: 'Tarth',
-      location: 'Tarth',
-    },
-    {
-      name: 'Helen of Patron',
-      shortCode: 'Porgy',
-      actor: 'Anna Gratia',
-      house: 'Golly',
-      location: 'Golly',
-    },
-  ],
-};
-
 jQuery(document).ready(() => {
   // document ready message
   jQuery('footer small').text('Ready to Manipulate!');
 
   // get html template and compile date
-  let character = Handlebars.compile(jQuery('#character-template').text());
+  let compiledTemplate = Handlebars.compile(
+    jQuery('#character-template').text(),
+  );
 
-  // render template
-  jQuery('.character-list-container').html(character(cast));
+  // get data with AJAX, when don, render the template
+  jQuery.ajax('data/cast.json').done(cast => {
+    jQuery('.character-list-container').html(compiledTemplate(cast));
+  });
 });
